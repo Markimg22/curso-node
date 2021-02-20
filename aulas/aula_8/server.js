c5ea85c3f98292de630d288cafcb30f1b09b33bc
Markimg22 <marquim.net.221200@hotmail.com>
2021-02-20 09:26:11 -0400

    connection to database

--- aulas/aula_8/server.js
+++ aulas/aula_8/server.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+// Connection to Database
+const mongoose = require('mongoose');
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connection to database finished');
+    app.emit('Connected');
+  })
+  .catch((e) => console.log(`Error in connection: ${e}`));
+
 const routes = require('./routes');
 const path = require('path');
 const middleware = require('./src/middlewares/middleware');
 
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
@@ -15,6 +30,9 @@ app.set('view engine', 'ejs');
 app.use(middleware); // todas as requisições vão passar por essa função
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Server starting in http://localhost:3000/');
+// Connected
+app.on('Connected', () => {
+  app.listen(3000, () => {
+    console.log('Server starting in http://localhost:3000/');
+  });
 });
